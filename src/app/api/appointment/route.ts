ed7abd0206b8bd6ff68c5461baabea5c469d6df7
jjxmonster <kuba.tarabasz@wp.pl>
2023-11-05 15:22:30 +0100

    remove useless api endpoints

--- src/app/api/appointment/route.ts
+++ src/app/api/appointment/route.ts
@@ -1,9 +1,7 @@
-import { AppointmentPayload, UserPayload } from "types/common";
+import { AppointmentPayload } from "types/common";
 
-import { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 import createAppointment from "services/appointment/create";
-import { getAppointmentForUser } from "services/appointment/get";
 
 export async function POST(req: Request, _res: NextResponse) {
   try {
@@ -19,19 +17,5 @@ export async function POST(req: Request, _res: NextResponse) {
       { status: "not_created", error: error.message },
       { status: 400 }
     );
-  }
-}
-
-export async function GET(req: NextApiRequest, _res: Response) {
-  try {
-    const url = new URL(req.url as string);
-    const searchParams = new URLSearchParams(url.search);
-    const email = searchParams.get("email");
-
-    const appointment = await getAppointmentForUser(email ?? "");
-
-    return NextResponse.json({ appointment }, { status: 200 });
-  } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 400 });
   }
 }
